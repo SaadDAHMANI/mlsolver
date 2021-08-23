de80d8c933d4e776ae432d826ebf26326f87840d
SaadDAHMANI <tahatatakor7@gmail.com>
2021-08-23 17:22:54 +0100

    add Qmax and main loop.
    Signed-off-by: SaadDAHMANI <tahatatakor7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,71 @@
 // Developped by : Saad Dahmani <sd.dahmani2000@gmail.com; s.dahmani@univ-bouira.dz>
 //***********************************************************************************************
 
+include!("mlsolver.rs");
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, mlsolver!");
+
+    // MATLAB like matrix constructor
+    let a = ml_matrix("1 2;3 4");
+
+    // R like matrix constructor (default)
+    let b = matrix(c!(1,2,3,4), 2, 2, Row);
+
+    // Or use zeros
+    let mut z = zeros(2, 2);
+    z[(0,0)] = 1.0;
+    z[(0,1)] = 2.0;
+    z[(1,0)] = 3.0;
+    z[(1,1)] = 4.0;
+
+    // Simple but effective operations
+    let c = a * b; // Matrix multiplication (BLAS integrated)
+
+    // Easy to pretty print
+    c.print();
+    //       c[0] c[1]
+    // r[0]     1    3
+    // r[1]     2    4
+
+    // Easy to do linear algebra
+    c.det().print();
+    c.inv().print();
+
+    // and etc.
+
+   let (a21, a01, q, r) = network1();
+     
+   ml_solver(&a21, &a01, &q, &r);
 
 
+   println!("rows = {}, cols ={}", q.row, q.col )
 
+
+}
+
+fn network1()->(Matrix, Matrix, Matrix, Matrix) {
+    //let nn =4;
+    //let np = 7;
+    //let mut a21 = zeros(nn,np);
+    let a21 = ml_matrix("1 0 -1 -1 -1 0 0; 
+                         0 1 1 0 0 -1 0; 
+                         0 0 0 1 0 0 -1;
+                         0 0 0 0 1 1 1");
+     
+       
+    let mut a01 = ml_matrix("-1 -1 0 0 0 0 0");
+    a01=a01.transpose();
+    //demand vector 
+    let mut q = ml_matrix("0.1 0.2 0.3 0.4");
     
+     q = q.transpose();
+    
+    let mut r = ml_matrix("1.5625 50 100 12.5 75 200 100");
+    r=r.transpose();
+
+    (a21, a01, q, r) 
+
+
+
 }
--- src/mlsolver.rs
+++ src/mlsolver.rs
@@ -5,3 +5,88 @@
 // Developped by : Saad Dahmani <sd.dahmani2000@gmail.com; s.dahmani@univ-bouira.dz>
 //***********************************************************************************************
 
+#[macro_use]
+extern crate peroxide;
+use peroxide::prelude::*;
+//use peroxide::fuga::*;
+
+pub fn ml_solver(a21 : &Matrix, a01 : &Matrix, q : &Matrix, r : &Matrix){
+
+let nn =a21.row;
+let np =a21.col;
+
+if nn<2 {return;}
+if np<1 {return;}
+
+let mut iter : usize =0;
+let itermax : usize=1; 
+//let err : f64 =10f64;
+//let errobj : f64 =0.0001;
+
+
+
+// step 0 : compute Qmax 
+let mut qmax : f64 =0.0;
+for i in 0..q.row {
+    qmax += q[(i,0)]; 
+}
+
+// step 1 : establish A & B using eq.19
+// 1.1- initilize A matrix :
+let a= initilize_a(&r, qmax);
+
+a.print();
+
+// 1.2- initiliza B matrix :
+let b = zeros(np,1);
+
+b.print();
+
+
+//while err>errobj {
+while iter < itermax {
+    iter+=1;
+
+  // step 1- Update A & B
+
+  // step 2- Compute V (eq.31) and C (eq.32)
+  
+  let inva= a.transpose().inv();
+
+  //let v = inv_a*a21;
+
+  println!("[A]^-1 = a.inv()");
+  inva.print();
+
+  let v = prouct(&a21, &inva);
+
+
+}
+
+
+
+
+
+}
+
+fn initilize_a(resistance : &Matrix, qmax : f64)->Matrix {
+   
+    let np = resistance.row;
+
+    let mut a = eye(np);
+ 
+    for i in 0..np {
+        a[(i,i)]=resistance[(i,0)]*qmax;    
+    }
+
+    return a;
+}
+
+
+fn prouct(left : &Matrix, right : &Matrix)->Matrix{
+
+    let nr =  left.row;
+    let result = zeros(nr,nr);
+    result
+
+}
